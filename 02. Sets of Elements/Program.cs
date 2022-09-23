0e105e45d99def3c9a2bbbbd24cc99f55bd3b93d
hasankr <hasankr@abv.bg>
2022-09-23 21:33:14 +0300

    added 2

---
+++ 02. Sets of Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+            int[] counts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < counts[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < counts[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+            firstSet.IntersectWith(secondSet);
+            Console.WriteLine(string.Join(" ", firstSet));
+        }
+    }
+}
