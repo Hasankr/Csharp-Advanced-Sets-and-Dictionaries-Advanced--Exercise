967141ae15019f51cd50e9e2b08099b8fdd645f2
hasankr <hasankr@abv.bg>
2022-09-23 21:18:33 +0300

    Add project files.

---
+++ 01. Unique Usernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            HashSet<string> userNames = new HashSet<string>();
+            for (int i = 0; i < count; i++)
+            {
+                userNames.Add(Console.ReadLine());
+            }
+            foreach (var userName in userNames)
+            {
+                Console.WriteLine(userName);
+            }
+        }
+    }
+}
