81c8e85a47a752c486b5a388873a674cd58b1e86
hasankr <hasankr@abv.bg>
2022-09-23 21:50:35 +0300

    added 3

---
+++ 03. Periodic Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> periodicElements = new SortedSet<string>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+                periodicElements.UnionWith(elements);
+            }
+            Console.WriteLine(string.Join(" ", periodicElements));
+        }
+    }
+}
